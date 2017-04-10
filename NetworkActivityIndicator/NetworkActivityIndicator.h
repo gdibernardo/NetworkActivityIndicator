149eb971c9e9477654e89cc79880f88c9e8579d7
Gabriele Di Bernardo <gabriele.dibernardo@me.com>
2017-04-11 01:12:12 +0200

    Initial commit

---
+++ NetworkActivityIndicator/NetworkActivityIndicator.h
@@ -0,0 +1,19 @@
+//
+//  NetworkActivityIndicator.h
+//  NetworkActivityIndicator
+//
+//  Created by Gabriele Di Bernardo on 11/04/2017.
+//  Copyright © 2017 Gabriele Di Bernardo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NetworkActivityIndicator.
+FOUNDATION_EXPORT double NetworkActivityIndicatorVersionNumber;
+
+//! Project version string for NetworkActivityIndicator.
+FOUNDATION_EXPORT const unsigned char NetworkActivityIndicatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetworkActivityIndicator/PublicHeader.h>
+
+
---
+++ NetworkActivityIndicator/NetworkActivityIndicator.swift
@@ -0,0 +1,86 @@
+//
+//  NetworkActivityIndicator.swift
+//  NetworkActivityIndicator
+//
+//  Created by Gabriele Di Bernardo on 11/04/2017.
+//  Copyright © 2017 Gabriele Di Bernardo. All rights reserved.
+//
+
+
+import Foundation
+import UIKit
+
+
+
+class NetworkActivityIndicator: NSObject
+{
+    private static let instance = NetworkActivityIndicator()
+    
+    private var requestedActivities: Int = 0
+    
+    
+    static func sharedInstance() -> NetworkActivityIndicator
+    {
+        return instance
+    }
+    
+    
+    func show()
+    {
+        self.lock()
+        
+        defer {
+            self.unlock()
+        }
+        
+        self.requestedActivities += 1
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+    }
+    
+    
+    func hide()
+    {
+        self.lock()
+        
+        defer {
+            self.unlock()
+        }
+        
+        if(self.requestedActivities > 0)
+        {
+            self.requestedActivities -= 1
+            if(self.requestedActivities == 0)
+            {
+                UIApplication.shared.isNetworkActivityIndicatorVisible = false
+            }
+        }
+    }
+    
+    
+    func isOnScreen() -> Bool
+    {
+        self.lock()
+        defer {
+            self.unlock()
+        }
+        
+        if(self.requestedActivities > 0)
+        {
+            return true
+        }
+        else
+        {
+            return false
+        }
+    }
+    
+    
+    private func lock() {
+        objc_sync_enter(self)
+    }
+    
+    
+    private func unlock() {
+        objc_sync_exit(self)
+    }
+}
